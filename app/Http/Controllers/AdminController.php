d2d8ec5acaaa981711a1fc44b63137e68dccfcf2
Dieter Burgstaller <dburgstaller@gmx.de>
2024-08-26 16:27:57 +0200

    #14 Laravel Multi Auth with Breete Part 2 -- S3 Multi Auth with Breeze Create Auth for User/ Instructor /Admin -- Laravel 11 Build Multi Restaurant

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    /**
+     * Admin Login Function
+     *
+     * @return void
+     */
+    public function AdminLogin(){
+        return view('admin.login');
+
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function AdminDashboard(){
+        return view('admin.admin_dashboard');
+
+    }
+}
